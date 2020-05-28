578c9c525885949b0a9d8b604d7743308b9980f3
Arlena McClenton <arlenamcclenton@gmail.com>
2020-05-27 20:41:06 -0400

    created functions for operations

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 
 //add function
+function add(n1, n2){
+	return n1 + n2;
+}
 //subtract function
+function subtract(n1, n2){
+	return n1 - n2;
+}
+
 //multiply function
+function multiply(n1, n2){
+	return n1 * n2;
+}
 //divide function
+function divide(n1, n2){
+	return n1/n2;
+}
 //calculate function
 //erase function
