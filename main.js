fccd84ce95e29a7387b96707c790265e44a4297f
Arlena McClenton <arlenamcclenton@gmail.com>
2020-06-06 09:18:21 -0400

    added functions

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+let addBtn = document.getElementsByClassName("addBtn") //.addEventListener("click", add, false);
+let subBtn = document.getElementsByClassName("subBtn");
+let divBtn = document.getElementsByClassName("divBtn");
+let mulBtn = document.getElementsByClassName("mulBtn");
+let numBtns = document.getElementsByClassName("numButn") //.addEventListener("click", getOperands, false);
+
+operands = [];
+operations = [];
+
+//TODO: make function to push numBtns into operands array as they're clicked
+
+function formatNumButtons(){
+
+}
+
 
 //add function
 function add(n1, n2){
@@ -19,3 +34,6 @@ function divide(n1, n2){
 //calculate function
 //erase function
 
+function getOperands(){
+
+}
